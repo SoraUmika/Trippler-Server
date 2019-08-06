4ce4d177d875ce77508ef519e5aa052ccbc69cd7
SoraUmika <dazzlejohn1212@gmail.com>
2019-08-06 00:22:00 -0400

    update

--- src/routes/database.ts
+++ src/routes/database.ts
@@ -19,6 +19,7 @@ databaseRoute.post('/', (request, response) => {
         case 'grab_random_bussiness':
             grab_random_business((err, result) => {
                 if (err){
+                    console.log("FAIL TO RECEIVE DATA FROM DATABASE: ", err)
                     response.send({ErrorFromServer: "Requests received, but is unable to retrieve data from server database: ", err})
                 }else{
                     console.log("sending_packings to: ", request.headers['x-forwarded-for'] || request.connection.remoteAddress) 
